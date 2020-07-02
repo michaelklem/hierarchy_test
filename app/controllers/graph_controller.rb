589f892d13e977e45c9364fa0c3b25448d054979
Michael Klem <michael.klem@gmail.com>
2020-07-02 16:56:18 -0700

    Removing tips for ajax

--- app/controllers/graph_controller.rb
+++ app/controllers/graph_controller.rb
@@ -1,18 +1,16 @@
 class GraphController < ApplicationController
   def index
     @nodes = AiAccount.all_with_links
-    # @nodes = AiAccount.all.pluck(:id, :account_type)
     @edges = AiAccountParent.all
   end
 
   def node_hierarchy
-    Rails.logger.info "PPPPP #{params[:node_id]}"
     @node_id = params[:node_id]
   end
 
   def node_hierarchy_data
     @nodes = AiAccount.get_parent_child_nodes( params['node_id'], 4, nil)
-    Rails.logger.info "NODES: #{@nodes.to_json}"
+    # Rails.logger.info "NODES: #{@nodes.to_json}"
     uniq_nodes1 = @nodes.map{|x| x['account_id']}.uniq
     uniq_nodes2 = @nodes.map{|x| x['parent_id']}.uniq
     uniq_nodes = uniq_nodes1 + uniq_nodes2
@@ -20,9 +18,9 @@ class GraphController < ApplicationController
 
     # @nodes = AiAccount.all.pluck(:id, :account_type)
 
-    Rails.logger.info "NODES2: #{@nodes.to_json}"
-    Rails.logger.info "EDGES: #{@edges.to_json}"
-    Rails.logger.info "uniq_nodes: #{uniq_nodes}"
+    # Rails.logger.info "NODES2: #{@nodes.to_json}"
+    # Rails.logger.info "EDGES: #{@edges.to_json}"
+    # Rails.logger.info "uniq_nodes: #{uniq_nodes}"
     render :json => {nodes: @nodes, edges: @edges, uniq_nodes: uniq_nodes, node_ids: uniq_nodes1}
   end
 end
