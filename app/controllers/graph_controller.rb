012ef4fbd5c6fbd2176edc4aa14ed6969c24d88d
Michael Klem <michael.klem@gmail.com>
2020-07-06 09:50:37 -0700

    Renders a single parent node to specific depth by default

--- app/controllers/graph_controller.rb
+++ app/controllers/graph_controller.rb
@@ -1,7 +1,43 @@
 class GraphController < ApplicationController
+  # Find all nodes to a specific depth for a specific root
   def index
+      @edges = AiAccount.get_parent_child_nodes3( '22897', 7 )
+
+      # get unique node ids
+      account_ids = @edges.map{|x| x['account_id']}
+      parent_ids = @edges.map{|x| x['parent_id']}
+      node_ids = (account_ids + parent_ids).uniq.compact
+      @nodes = AiAccount.links_for_nodes(node_ids.join(','))
+      # @edges = AiAccountParent.all
+      
+      # n = AiAccount.first
+      # @nodes = [[n.id, n.account_type]]
+      # @node_ids = []
+      # @nodes.map{|x| @node_ids << x[0]}
+
+      # @node_ids = [[22897]]
+
+
+      
+      Rails.logger.info "DONE!"
+    end
+    
+    def index2
     @nodes = AiAccount.all_with_links
-    @edges = AiAccountParent.all
+    # @edges = AiAccountParent.all
+    
+    # n = AiAccount.first
+    # @nodes = [[n.id, n.account_type]]
+    @node_ids = []
+    @nodes.map{|x| @node_ids << x[0]}
+
+    @node_ids = [[22897]]
+
+
+    # @edges = AiAccount.get_parent_child_nodes2( @node_ids.join(',') )
+    @edges = AiAccount.get_parent_child_nodes2( '22897', 2 )
+    
+    Rails.logger.info "DONE!"
   end
 
   def node_hierarchy
--- app/models/AiAccount.rb
+++ app/models/AiAccount.rb
@@ -2,6 +2,42 @@ class AiAccount < ApplicationRecord
   has_many :parents, primary_key: 'id',  foreign_key: 'parent_id', class_name: 'AiAccountParent'
   has_many :children, primary_key: 'id',  foreign_key: 'account_id', class_name: 'AiAccountParent'
 
+  def self.links_for_nodes(node_ids)
+    sql = "select distinct a.id, a.account_type from ai_accounts a
+	     join ai_accounts_parents aap2 on aap2.account_id = a.id
+       where a.id in (#{node_ids})
+       "
+    
+    # My hierachy generator currently does not insert all root nodes
+    # in the ai_accounts_parents table so I use this query logic to 
+    # find all nodes. Basically if the account id exists in either 
+    # column, it is a node that should be displayed. Some nodes
+    # are generated as orphans so we want to exclude those. 
+    sql5 = "
+	select distinct a.id, a.account_type from ai_accounts a
+	     join ai_accounts_parents aap on aap.parent_id = a.id
+	union
+	select distinct a.id, a.account_type from ai_accounts a
+	     join ai_accounts_parents aap2 on aap2.account_id = a.id
+	    "
+
+    sql4 = "select distinct a.id, a.account_type from ai_accounts a
+	    left join ai_accounts_parents aap on aap.parent_id = a.id
+	    left join ai_accounts_parents aap2 on aap2.account_id = a.id
+      "
+    sql3 = "select distinct a.id, a.account_type from ai_accounts a
+	    join ai_accounts_parents aap on aap.parent_id = a.id"
+
+    sql2 = "SELECT
+	 a2.id, a2.account_type
+FROM
+	classicmodels.ai_accounts_parents a1
+	join ai_accounts a2 on a2.id = a1.parent_id"
+    v = self.connection.select_rows(sql)
+    v
+  end
+
+
   def self.all_with_links
     sql = "select distinct a.id, a.account_type from ai_accounts a
 	     join ai_accounts_parents aap2 on aap2.account_id = a.id"
@@ -35,7 +71,7 @@ FROM
     v
   end
 
-  def self.get_parent_child_nodes(node_id, depth = 5, type = nil)
+  def self.get_parent_child_nodes(node_ids, depth = 5, type = nil)
     parent_depth = -depth # need a negative value for parents
 
     sql = "
@@ -47,7 +83,7 @@ FROM
       UNION ALL
       SELECT c.account_id, c.parent_id, cte.depth - 1, CONCAT(cte.path, \",\", c.account_id) FROM ai_accounts_parents c 
       JOIN cte ON c.account_id=cte.parent_id
-      where depth > -3
+      where depth > -#{depth}
     )
     SELECT  depth, cte.account_id, cte.parent_id, a.account_type, cte.path FROM cte
     join ai_accounts a on a.id = cte.account_id
@@ -71,10 +107,111 @@ FROM
 
       SELECT c.account_id, c.parent_id,cte.depth + 1, CONCAT(cte.path,\",\", c.account_id) FROM ai_accounts_parents c 
       JOIN cte ON cte.account_id=c.parent_id
-        where depth < 3
+        where depth < #{depth}
     )
 
     SELECT  depth, cte.account_id, cte.parent_id, a.account_type, cte.path FROM cte
+    join ai_accounts a on a.id = cte.account_id
+    left join ai_accounts p on p.id = cte.parent_id
+
+    )b"
+
+
+    Rails.logger.info "FFFFF #{sql}"
+    v = self.connection.select_all(sql)
+  end
+
+
+  def self.get_parent_child_nodes2(node_ids, depth = 1, type = nil)
+    parent_depth = -depth # need a negative value for parents
+
+    sql = "
+    select * from (
+    WITH RECURSIVE cte AS
+    (
+      SELECT account_id, parent_id, -1 as depth, CAST(account_id AS CHAR(200)) as path FROM ai_accounts_parents 
+      WHERE account_id  in (#{node_ids})
+      UNION ALL
+      SELECT c.account_id, c.parent_id, cte.depth - 1, CONCAT(cte.path, \",\", c.account_id) FROM ai_accounts_parents c 
+      JOIN cte ON c.account_id=cte.parent_id
+      where depth > -#{depth}
+    )
+    SELECT  depth, cte.account_id, cte.parent_id, a.account_type, cte.path FROM cte
+    join ai_accounts a on a.id = cte.account_id
+    left join ai_accounts p on p.id = cte.parent_id
+
+    )a
+
+    union all
+
+
+    select * from (
+
+
+
+    WITH RECURSIVE cte AS
+    (
+      SELECT account_id, parent_id, 0 as depth, CAST(account_id AS CHAR(200)) as path FROM ai_accounts_parents 
+      WHERE parent_id in (#{node_ids})
+
+      UNION ALL
+
+      SELECT c.account_id, c.parent_id,cte.depth + 1, CONCAT(cte.path,\",\", c.account_id) FROM ai_accounts_parents c 
+      JOIN cte ON cte.account_id=c.parent_id
+        where depth < #{depth}
+    )
+
+    SELECT  depth, cte.account_id, cte.parent_id, a.account_type, cte.path FROM cte
+    join ai_accounts a on a.id = cte.account_id
+    left join ai_accounts p on p.id = cte.parent_id
+
+    )b"
+
+
+    Rails.logger.info "FFFFF #{sql}"
+    v = self.connection.select_all(sql)
+  end
+
+ def self.get_parent_child_nodes3(node_ids, depth = 1, type = nil)
+    parent_depth = -depth # need a negative value for parents
+
+    sql = "
+    select * from (
+    WITH RECURSIVE cte AS
+    (
+      SELECT account_id, parent_id, -1 as depth FROM ai_accounts_parents 
+      WHERE account_id  in (#{node_ids})
+      UNION ALL
+      SELECT c.account_id, c.parent_id, cte.depth - 1 FROM ai_accounts_parents c 
+      JOIN cte ON c.account_id=cte.parent_id
+      where depth > -#{depth}
+    )
+    SELECT  depth, cte.account_id, cte.parent_id, a.account_type FROM cte
+    join ai_accounts a on a.id = cte.account_id
+    left join ai_accounts p on p.id = cte.parent_id
+
+    )a
+
+    union all
+
+
+    select * from (
+
+
+
+    WITH RECURSIVE cte AS
+    (
+      SELECT account_id, parent_id, 0 as depth FROM ai_accounts_parents 
+      WHERE parent_id in (#{node_ids})
+
+      UNION ALL
+
+      SELECT c.account_id, c.parent_id,cte.depth + 1 FROM ai_accounts_parents c 
+      JOIN cte ON cte.account_id=c.parent_id
+        where depth < #{depth}
+    )
+
+    SELECT  depth, cte.account_id, cte.parent_id, a.account_type FROM cte
     join ai_accounts a on a.id = cte.account_id
     left join ai_accounts p on p.id = cte.parent_id
 
